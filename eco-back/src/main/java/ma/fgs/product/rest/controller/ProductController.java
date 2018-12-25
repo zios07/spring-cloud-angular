f49c94297fcb21a94463bd52a61688e3fcf114be
zios07 <zakaria.kaoukab@mail.com>
2018-12-25 23:17:02 +0100

    add compose for postgres db and adminer

--- eco-back/src/main/java/ma/fgs/product/rest/controller/ProductController.java
+++ eco-back/src/main/java/ma/fgs/product/rest/controller/ProductController.java
@@ -5,6 +5,7 @@ import ma.fgs.product.domain.dto.ProductSearchDto;
 import ma.fgs.product.service.api.IProductService;
 import ma.fgs.product.service.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@RefreshScope
 @RequestMapping("api/v1/products")
 public class ProductController {
 
