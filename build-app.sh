cc8ec3304fd5089a8f7054a354fc80090ad955fd
zios07 <zakaria.kaoukab@mail.com>
2018-12-29 21:30:51 +0100

    add shell file to build image

---
+++ build-app.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "building discovery-server"
+
+cd discovery-server
+mvn clean package -DskipTests
+
+echo "building edge-server"
+
+cd ../edge-server
+mvn clean package -DskipTests
+
+echo "building config-server"
+
+cd ../config-server
+mvn clean package -DskipTests
+
+echo "building product-service"
+
+cd ../eco-back
+mvn clean package -DskipTests
+
+echo ""
+echo "Building images ..."
+echo ""
+echo "Building image for discovery-server"
+echo ""
+
+cd discovery-server
+docker build -t zios07/discovery-server .
+
+
+echo ""
+echo "Building image for edge-server"
+echo ""
+
+docker build -t zios07/edge-server ../edge-server
+
+
+echo ""
+echo "Building image for config-server"
+echo ""
+
+docker build -t zios07/config-server ../config-server
+
+
+echo ""
+echo "Building image for product-server"
+echo ""
+
+docker build -t zios07/product-service ../eco-back
+exit;
