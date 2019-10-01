9ff0f9a8de62e007593c647c40d3345b35e52a94
zios07 <zakaria.kaoukab@gmail.com>
2019-10-01 22:33:34 +0100

    roles creation on startup + add host property to mongodb config

--- eco-back/src/main/java/ma/fgs/product/ProductServiceApplication.java
+++ eco-back/src/main/java/ma/fgs/product/ProductServiceApplication.java
@@ -1,18 +1,41 @@
 package ma.fgs.product;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
+import ma.fgs.product.domain.Role;
 import ma.fgs.product.publisher.MessagePublisher;
+import ma.fgs.product.repository.RoleRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableBinding(MessagePublisher.class)
-public class ProductServiceApplication {
+public class ProductServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	public ProductServiceApplication(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (roleRepository.count() == 0) {
+			Role role1 = new Role("ADMIN", "Administrator");
+			Role role2 = new Role("USER", "User");
+			Role role3 = new Role("SELLER", "Seller");
+			roleRepository.saveAll(Arrays.asList(role1, role2, role3));
+		}
 	}
 }
