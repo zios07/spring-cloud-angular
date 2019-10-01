b57d3c2e08b340cc08ae2e8b2ed76cad26df842c
zios07 <zakaria.kaoukab@gmail.com>
2019-10-01 22:55:29 +0100

    fix mongo connection and attachment size exception

--- eco-web/src/app/components/admin/product/add-product/add-product.component.ts
+++ eco-web/src/app/components/admin/product/add-product/add-product.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
-import { ProductService } from '../../../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UUID } from 'angular2-uuid';
 import { ToastrService } from 'ngx-toastr';
-import { Product } from '../../../../domain/product';
-import { BrandService } from '../../../../services/brand.service'
 import 'rxjs/add/operator/take';
 import { Brand } from '../../../../domain/brand';
-import { UUID } from 'angular2-uuid';
 import { Category } from '../../../../domain/category';
+import { Product } from '../../../../domain/product';
+import { BrandService } from '../../../../services/brand.service';
 import { CategoryService } from '../../../../services/category.service';
+import { ProductService } from '../../../../services/product.service';
 
 @Component({
-  selector: 'app-add-product',
-  templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+	selector: 'app-add-product',
+	templateUrl: './add-product.component.html',
+	styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
 
 	brands: Array<Brand> = [];
 	categories: Array<Category> = [];
 	product: Product = new Product();
-	attachments: any[] = [];
+	attachments: any[];
 
 	constructor(private router: Router,
-				private route: ActivatedRoute,
-				private productService: ProductService,
-				private brandService: BrandService,
-				private categoryService: CategoryService,
-				private toastr: ToastrService) {}
+		private route: ActivatedRoute,
+		private productService: ProductService,
+		private brandService: BrandService,
+		private categoryService: CategoryService,
+		private toastr: ToastrService) { }
 
 	ngOnInit() {
 		this.loadBrands();
@@ -36,28 +36,28 @@ export class AddProductComponent implements OnInit {
 	}
 
 	uploadPhotos(event) {
-		if(event.files)
-			if (event.files.length < 3 || event.files.length > 7)
-				this.toastr.error("You must select between 3 and 7 images");
-			else{
-				// this.productService.uploadPhotos(event.files).subscribe(result => {
-				// 	console.log(result);
-				// }, error => {
-				// 	this.toastr.error(String(error));
-				// });
-				this.attachments.push(event.files);
+		if (event.files) {
+			if (event.files.length < 3 || event.files.length > 7) {
+				this.toastr.error('You must select between 3 and 7 images');
+			} else {
+				if (this.attachments) {
+					this.attachments.push(event.files[0]);
+				} else {
+					this.attachments = event.files;
+				}
 			}
+		}
 	}
 
-	saveProduct(){
+	saveProduct() {
 		this.productService.save(this.product, this.attachments).subscribe(result => {
 			this.toastr.success('Product added successfully !');
 			this.router.navigate(['/admin/products']);
 		});
 	}
 
-	loadBrands(){
-		this.brandService.loadBrands().subscribe((result:Array<Brand>) => {
+	loadBrands() {
+		this.brandService.loadBrands().subscribe((result: Array<Brand>) => {
 			this.brands = result;
 			this.loadProductToEdit();
 		}, error => {
@@ -65,8 +65,8 @@ export class AddProductComponent implements OnInit {
 		})
 	}
 
-	loadCategories(){
-		this.categoryService.loadCategories().subscribe((result:Array<Category>) => {
+	loadCategories() {
+		this.categoryService.loadCategories().subscribe((result: Array<Category>) => {
 			this.categories = result;
 			this.loadProductToEdit();
 		}, error => {
@@ -74,17 +74,17 @@ export class AddProductComponent implements OnInit {
 		})
 	}
 
-	loadProductToEdit(){
-		let id = this.route.snapshot.paramMap.get('id');
+	loadProductToEdit() {
+		const id = this.route.snapshot.paramMap.get('id');
 		if (id) {
 			this.productService.getProduct(id).take(1).subscribe((result: Product) => {
 				this.product = result;
-				// There has to be a better way to do the 2 way binding .. 
-				if(this.brands) {
+				// There has to be a better way to do the 2 way binding ...
+				if (this.brands) {
 					const p = this.product;
 					let match;
-					this.brands.forEach(function(br){
-						if(br.id == p.brand.id){
+					this.brands.forEach(function (br) {
+						if (br.id === p.brand.id) {
 							match = br;
 						}
 					})
@@ -97,9 +97,8 @@ export class AddProductComponent implements OnInit {
 	}
 
 	generateUUID() {
-		localStorage.removeItem("uuid");
-		let uuid = UUID.UUID();
-		localStorage.setItem("uuid", uuid);
+		localStorage.removeItem('uuid');
+		localStorage.setItem('uuid', UUID.UUID());
 	}
 
 }
--- eco-web/src/app/services/product.service.ts
+++ eco-web/src/app/services/product.service.ts
@@ -1,68 +1,62 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import "rxjs/add/operator/map";
-import { environment } from "../../environments/environment";
-import { CartDto } from "../dto/cartDto";
-import { AuthenticationService } from "./authentication.service";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
+import { CartDto } from '../dto/cartDto';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class ProductService {
 
     url: string = environment.API_URL;
 
-    constructor(private http: HttpClient, 
-                private authService: AuthenticationService) { }
+    constructor(private http: HttpClient,
+        private authService: AuthenticationService) { }
 
     loadProducts(page, size) {
-        return this.http.get(this.url + "/api/v1/products?page="+page+"&size="+size)
+        return this.http.get(this.url + '/api/v1/products?page=' + page + '&size=' + size)
             .map((response: any) => response.content);
     }
 
-    getProduct( id ) {
-        return this.http.get(this.url + "/api/v1/products/"+id)    
+    getProduct(id) {
+        return this.http.get(this.url + '/api/v1/products/' + id)
             .map(response => response);
     }
 
-    // addProduct(product) {
-    //     product.uuid = localStorage.getItem('uuid');
-    //     return this.http.post(this.url + "/api/v1/products", product)
-    //         .map(response => response);
-    // }
-
     addProductToCart(product, username) {
-        let dto = new CartDto(username, product);
-        return this.http.put(this.url + "/api/v1/cart/product/add", dto)
+        const dto = new CartDto(username, product);
+        return this.http.put(this.url + '/api/v1/cart/product/add', dto)
             .map(response => response);
     }
 
     minusProductFromCart(product, username) {
-        let dto = new CartDto(username, product);
-        return this.http.put(this.url + "/api/v1/cart/product/minus", dto)
+        const dto = new CartDto(username, product);
+        return this.http.put(this.url + '/api/v1/cart/product/minus', dto)
             .map(response => response);
     }
 
     deleteProductFromCart(product, username) {
-        return this.http.delete(this.url + "/api/v1/cart/product/delete?productid="+product.id+"&username="+username)
+        return this.http.delete(this.url + '/api/v1/cart/product/delete?productid=' + product.id + '&username=' + username)
             .map(response => response);
     }
 
     search(dto, page, size) {
-        return this.http.post(this.url + "/api/v1/products/search?page="+page+"&size="+size , dto)
+        return this.http.post(this.url + '/api/v1/products/search?page=' + page + '&size=' + size, dto)
             .map((response: any) => response.content);
     }
 
     save(product, photos) {
-        let fd = new FormData();
-        fd.append("product", JSON.stringify(product));
-        for(let i = 0 ; i < photos.length ; i++ ) {
-            var blob = new Blob([photos[i]], { type: "application/json"});
-            fd.append('attachments', blob , photos[i].name);
+        const fd = new FormData();
+        fd.append('product', JSON.stringify(product));
+        for (let i = 0; i < photos.length; i++) {
+            const blob = new Blob([photos[i]], { type: 'application/json' });
+            fd.append('attachments', blob, photos[i].name);
         }
-        return this.http.post(this.url + "/api/v1/products", fd)
+        return this.http.post(this.url + '/api/v1/products', fd)
             .map(response => response);
     }
 
     deleteProduct(id) {
-        return this.http.delete(this.url + "/api/v1/products/" + id)
+        return this.http.delete(this.url + '/api/v1/products/' + id);
     }
 }
