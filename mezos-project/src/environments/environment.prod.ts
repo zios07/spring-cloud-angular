45167edcad5262878da79013fac3428cb07bdc7d
zios07 <zakaria.kaoukab@gmail.com>
2019-10-22 17:22:26 +0100

    fix build issues related to production environment file

--- mezos-project/src/environments/environment.prod.ts
+++ mezos-project/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
 	API_URL: 'https://localhost:9901/',
-	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
+	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
+	keycloakConfig: null
 };
