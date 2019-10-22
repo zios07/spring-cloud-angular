aff22576bb28d7ee5a53e962cb8b049953dcd9fc
zios07 <zakaria.kaoukab@gmail.com>
2019-10-22 16:36:45 +0100

    update docker compose databases file to include keycloak

--- eco-back/src/main/java/ma/fgs/product/configuration/SecurityConfig.java
+++ eco-back/src/main/java/ma/fgs/product/configuration/SecurityConfig.java
@@ -5,17 +5,23 @@ import static ma.fgs.product.security.utils.SecurityConstants.REGISTRATION_URL;
 
 import java.util.Arrays;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,32 +33,48 @@ import ma.fgs.product.service.AccountService;
 import ma.fgs.product.service.UserService;
 import ma.fgs.product.service.api.IUserService;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@KeycloakConfiguration
+class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+	/**
+	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
+
+	/**
+	 * Provide a session authentication strategy bean which should be of type
+	 * RegisterSessionAuthenticationStrategy for public or confidential applications
+	 * and NullAuthenticatedSessionStrategy for bearer-only applications.
+	 */
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	/**
+	 * Use properties in application.properties instead of keycloak.json
+	 */
+	@Bean
+	public KeycloakConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors()
-			.and()
-				.csrf()
-					.disable()
-				.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-				.authorizeRequests()
-					.antMatchers("public/**")
-						.permitAll()
-					.antMatchers(HttpMethod.POST, LOGIN_URL, REGISTRATION_URL)
-						.permitAll()
-					.antMatchers(HttpMethod.OPTIONS)
-						.permitAll()
-					.anyRequest()
-					// TODO only permit authenticated requests
-						.permitAll()
-			.and()
-				.addFilter(jwtAuthenticationFilter())
+		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeRequests().antMatchers("public/**").permitAll()
+				.antMatchers(HttpMethod.POST, LOGIN_URL, REGISTRATION_URL).permitAll().antMatchers(HttpMethod.OPTIONS)
+				.permitAll().anyRequest()
+				// TODO only permit authenticated requests
+				.permitAll().and().addFilter(jwtAuthenticationFilter())
 				.addFilter(new JwtAuthorizationFilter(authenticationManager()));
 	}
 
@@ -60,10 +82,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		// the user details service and the password encoder to be user in the
-		// authenticated() method of authenticationManager of the JwtAuthentication Filter
+		// authenticated() method of authenticationManager of the JwtAuthentication
+		// Filter
 
-		auth.userDetailsService(userDetailsServiceBean())
-		 .passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(passwordEncoder());
 
 	}
 
@@ -84,7 +106,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public AccountService accountService() {
 		return new AccountService();
 	}
-	
+
 	@Bean
 	public IUserService userService() {
 		return new UserService();
