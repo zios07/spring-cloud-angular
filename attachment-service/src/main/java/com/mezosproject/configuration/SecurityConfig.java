fd441de9ccfe5352ef22dacc40c68ce6167d2c96
zios07 <zakaria.kaoukab@gmail.com>
2019-11-10 21:33:08 +0100

    secured product service

--- attachment-service/src/main/java/com/mezosproject/configuration/SecurityConfig.java
+++ attachment-service/src/main/java/com/mezosproject/configuration/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
@@ -21,7 +20,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @KeycloakConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
 	@Autowired
--- eco-back/src/main/java/ma/fgs/product/configuration/SecurityConfig.java
+++ eco-back/src/main/java/ma/fgs/product/configuration/SecurityConfig.java
@@ -1,113 +1,51 @@
 package ma.fgs.product.configuration;
 
-import static ma.fgs.product.security.utils.SecurityConstants.LOGIN_URL;
-import static ma.fgs.product.security.utils.SecurityConstants.REGISTRATION_URL;
-
 import java.util.Arrays;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import ma.fgs.product.security.JwtAuthenticationFilter;
-import ma.fgs.product.security.utils.UserDetailsServiceImpl;
-import ma.fgs.product.service.AccountService;
-import ma.fgs.product.service.UserService;
-import ma.fgs.product.service.api.IUserService;
-
 @KeycloakConfiguration
 class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-	/**
-	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
-	 */
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-
 		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-
 		auth.authenticationProvider(keycloakAuthenticationProvider);
 	}
 
-	/**
-	 * Provide a session authentication strategy bean which should be of type
-	 * RegisterSessionAuthenticationStrategy for public or confidential applications
-	 * and NullAuthenticatedSessionStrategy for bearer-only applications.
-	 */
 	@Bean
 	@Override
 	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+		return new NullAuthenticatedSessionStrategy();
 	}
 
-	/**
-	 * Use properties in application.properties instead of keycloak.json
-	 */
 	@Bean
-	public KeycloakConfigResolver KeycloakConfigResolver() {
-		return new KeycloakSpringBootConfigResolver();
+	@Primary
+	public KeycloakConfigResolver keycloakConfigResolver(KeycloakSpringBootProperties properties) {
+		return new CustomKeycloakSpringBootConfigResolver(properties);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().authorizeRequests().antMatchers("public/**").permitAll()
-				.antMatchers(HttpMethod.POST, LOGIN_URL, REGISTRATION_URL).permitAll().antMatchers(HttpMethod.OPTIONS)
-				.permitAll().anyRequest()
-				// TODO only permit authenticated requests
-				.authenticated();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// the user details service and the password encoder to be user in the
-		// authenticated() method of authenticationManager of the JwtAuthentication
-		// Filter
-
-		auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(passwordEncoder());
-
-	}
-
-	@Override
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-		return new UserDetailsServiceImpl(userService());
-	}
-
-	@Bean
-	public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-		final JwtAuthenticationFilter filter = new JwtAuthenticationFilter(userService());
-		filter.setAuthenticationManager(authenticationManager());
-		filter.setFilterProcessesUrl(LOGIN_URL);
-		return filter;
-	}
-
-	@Bean
-	public AccountService accountService() {
-		return new AccountService();
-	}
-
-	@Bean
-	public IUserService userService() {
-		return new UserService();
+		super.configure(http);
+		http.authorizeRequests().antMatchers("/**").authenticated().anyRequest().authenticated();
 	}
 
 	@Bean
@@ -123,7 +61,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	}
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
